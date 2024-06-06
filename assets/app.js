4ca0f8de7c09fc4d8c3f8aa9592893b5e5548989
hugo bellin <hugobellin@yahoo.com>
2024-06-06 04:16:24 +0200

    feat(all) start refonte css

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+// import './bootstrap.js';
 // 
 
 /*
@@ -15,4 +15,7 @@ import './img/hugo_bellin_cv.pdf'
 import './img/monster_website.jpg'
 import './img/bibliotheque.png'
 import './img/sejour.png'
+import './img/background.png'
+import './img/background-html.jpg'
+import './img/background-mobile.png'
 
